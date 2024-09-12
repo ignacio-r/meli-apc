fa43cdc84b18768e5f741f218751e9a3d4782624
Angelo Padron <padron891@gmail.com>
2024-09-12 16:47:43 -0300

    add new fields to response bodies and other minor changes

--- src/main/kotlin/org/meliapp/backend/dto/meli/MeliSearchResponse.kt
+++ src/main/kotlin/org/meliapp/backend/dto/meli/MeliSearchResponse.kt
@@ -1,13 +1,11 @@
 package org.meliapp.backend.dto.meli
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 import org.meliapp.backend.dto.product.ProductResponse
 
-data class MeliSearchResponse(
-    val results: List<ProductResponse>,
+data class MeliSearchResponse @JsonCreator constructor(
+    val results: List<ProductResponse> = emptyList(),
     @JsonProperty(value = "available_filters")
-    val availableFilters: List<SearchFilter>
-) {
-    // Default constructor for Jackson
-    constructor() : this(emptyList(), emptyList())
-}
+    val availableFilters: List<SearchFilter> = emptyList(),
+)
