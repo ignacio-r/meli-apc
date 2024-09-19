85559d1d445586e17d0c9b4663a2ff45df724146
ignacio-r <ignaciorobledounq@gmail.com>
2024-09-19 18:00:09 -0300

    add user entity and dependencies

---
+++ src/main/kotlin/org/meliapp/backend/model/User.kt
@@ -0,0 +1,12 @@
+package org.meliapp.backend.model
+
+import java.util.*
+data class User(
+    val id: UUID,
+    val email: String,
+    val password: String,
+    val role: Role
+)
+enum class Role {
+    USER, ADMIN
+}
